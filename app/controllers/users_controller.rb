20d1205ea4e99fc1f582ce77cd7259abac8baf7f
Anna Dolan <anna.dolan@gmail.com>
2016-10-25 11:36:25 -0600

    Adds some login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
   def new
     @user = User.new
   end
@@ -6,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
       flash[:success] = "New user created!"
     elsif User.find_by(email: @user.email)
@@ -25,5 +27,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password_digest)
+  end
+
+  def require_login
+    unless session[:user_id]
+      flash[:error] = "You must be logged in to do that!"
+      redirect_to login_path
+    end
   end
 end
